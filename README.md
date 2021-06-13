073c5ece6e963b2849b8da9a47fb8df971e7ab45
AbdullahElian1 <abodelian28@gmail.com>
2021-06-13 16:06:19 +0300

    finish lab06

--- README.md
+++ README.md
@@ -68,3 +68,39 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+# City Explorer
+
+**Author**: Abdullah Elian
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+this project allow the user to enter any country name then the map will appear to show the user that country on the map## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+just the user lunch this app address and it will work the link:
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+in this project i used React.js framework also i used bootstrap and for the backend i installed the aioxs library to connect with API server . 
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+
+## Credit and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+
+
+Name of feature: Lab 06
+
+Estimate of time needed to complete: _2 hours
+
+Start time: 2 pm
+
+Finish time: 4pm
+
+Actual time needed to complete: 3:30
--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import Form from './component/Form'
+import Alert from 'react-bootstrap/Alert'
+import Figure from 'react-bootstrap/Figure'
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      locData: '',
+      displayErr: false,
+      displayMap: false,
+    }
+  }
+
+  explore = async (event) => {
+    try {
+      event.preventDefault();
+      let cityName = event.target.city.value;
+      console.log(cityName);
+      let url = `https://us1.locationiq.com/v1/search.php?key=pk.b88f218efa97772dabbe983a8f363988&q=${cityName}&format=json`;
+      let locResult = await axios.get(url);
+      console.log(locResult.data[0]);
+      this.setState({
+        locData: locResult.data[0],
+        displayMap: true
+      })
+    }
+    catch {
+
+      this.setState({
+        displayErr: true,
+
+      })
+
+    }
+
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Form showInfo={this.explore} />
+
+        
+
+        {this.state.displayMap &&<Figure>
+          <Figure.Image
+            width={400}
+            height={500}
+            alt="171x180"
+            src= {`https://maps.locationiq.com/v3/staticmap?key=pk.b88f218efa97772dabbe983a8f363988&center=${this.state.locData.lat},${this.state.locData.lon}`}  
+          />
+          <Figure.Caption>
+          <Alert variant='info'>
+          {this.state.locData && this.state.locData.display_name}
+        </Alert>
+          </Figure.Caption>
+        </Figure>}
+
+        {this.state.displayErr && 'error'}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
