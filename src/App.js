c72e2a28677996aedaf18c3614aea07e384f1cef
AbdullahElian1 <abodelian28@gmail.com>
2021-06-15 19:33:46 +0300

    add lab08

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Form from './component/Form'
 import Show from './component/Show'
 import Weather from './component/Weather';
+import Movie from './component/Movie'
 
 
 class App extends React.Component {
@@ -13,17 +14,19 @@ class App extends React.Component {
     this.state = {
       locData: '',
       cityInfo:{},
-      wheathr:{},
+      wheathr:[],
       displayErr: false,
       displayMap: false,
+      movies:[],
+      err:false
     }
   }
 
 
 
   explore = async (event) => {
+    event.preventDefault();
     try {
-      event.preventDefault();
       let cityName = event.target.city.value;
       console.log(cityName);
       let url = `https://us1.locationiq.com/v1/search.php?key=pk.b88f218efa97772dabbe983a8f363988&q=${cityName}&format=json`;
@@ -31,16 +34,20 @@ class App extends React.Component {
       let serverUrl=process.env.REACT_APP_SERVER;
       let weatherUrl=`${serverUrl}/getNames?cityLan=${locResult.data[0].lat}&cityLon=${locResult.data[0].lon}`;
       let weatherResult= await axios.get(weatherUrl);
-      console.log(weatherResult.data);
-      console.log(locResult.data[0].lat);
-      console.log(locResult.data[0].lon);
-
+      console.log(locResult.data);
+      let movieUrl= `http://localhost:3010/movies?cityName=${cityName}`;
+      //http://localhost:3010/movies?cityName=amman
+      //${serverUrl}/movies?cityName=
+      let movieResult= await axios.get(movieUrl);
+      console.log(movieResult);
       this.setState({
         cityInfo: locResult.data[0],
         wheathr: weatherResult.data,
-        displayMap: true
+        displayMap: true,
+        movies:movieResult.data,
+        err:true
       })
-      console.log(this.state.wheathr);
+      console.log(this.state.movies);
 
       
     }
@@ -64,7 +71,9 @@ class App extends React.Component {
         <Form showInfo={this.explore} />
         <Show map={this.state.displayMap} info={this.state.cityInfo} err={this.state.displayErr}  />
 
-        <Weather test={this.state.wheathr}/>
+        <Weather test={this.state.wheathr} err1={this.state.err}/>
+
+        <Movie mpvieList={this.state.movies} err5={this.state.err} />
 
       </div>
     )
